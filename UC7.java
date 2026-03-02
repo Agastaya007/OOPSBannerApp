4f37b1d940114f54f8cc7302f2ee91ae926a5578
Agastaya007 <agastayajindal007@gmail.com>
2026-03-02 22:55:22 +0530

    UC7

---
+++ UC7.java
@@ -0,0 +1,63 @@
+public class UC7 {
+
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char ch, String[] pat) {
+            character = ch;
+            pattern = pat;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CharacterPattern O = new CharacterPattern('O', new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern P = new CharacterPattern('P', new String[] {
+                " ***** ",
+                "*    * ",
+                "*    * ",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[] {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern[] word = { O, O, P, S };
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+            for (CharacterPattern cp : word) {
+                line.append(cp.getPattern()[row]).append("   ");
+            }
+            System.out.println(line);
+        }
+    }
+}
